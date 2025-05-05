32410d319271f0ff9bfb61bcd7a508ac6050c785
Frank R <frank.roesler@posteo.de>
2025-05-05 19:12:35 +0200

    update params

--- params.py
+++ params.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # TRAINING PARAMETERS:
 epochs = 10000
-lr = {"pulse": 1e-4, "gradient": 1e-3}  # learning rate
+lr = {"pulse": 1e-4, "gradient": 1e-4}  # learning rate
 plot_loss_frequency = 10  # plot every n steps
 start_logging = 1000  # start logging after n steps
 pre_train_inputs = False  # pre-train on given RF-pulse & gradient
@@ -20,13 +20,13 @@ model_args = {
     "n_coeffs": 30,  # Fourier Series
     "omega_0": 16,  # SIREN
     "bandwidth": 101,  # ModulatedFourier
-    "hidden_dim": 32,  # MLP, SIREN, ModulatedFourier
-    "num_layers": 16,  # MLP, SIREN
+    "hidden_dim": 64,  # MLP, SIREN, ModulatedFourier
+    "num_layers": 32,  # MLP, SIREN
     "num_centers": 10,  # RBFN
     "center_spacing": 1,  # RBFN
     "num_fourier_features": 51,  # FourierMLP
     "frequency_scale": 100.0,  # FourierMLP
-    "gradient_scale": 100.0,  # relative size of gradient to RF pulse
+    "gradient_scale": 200.0,  # relative size of gradient to RF pulse
     "positive_gradient": True,
     "tmin": t_B1[0].item(),
     "tmax": t_B1[-1].item(),
