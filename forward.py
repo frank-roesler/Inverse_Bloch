fix error in timeprof computation

forward.py
@@ -28,14 +28,14 @@ def forward(path, npts_some_b0_values=7, Nz=4096, Nt=512, npts_off_resonance=512
 
     freq_offsets_Hz = torch.linspace(-8000, 8000, npts_off_resonance)
     with torch.no_grad():
-        # export_param_csv(path, path, B1, G, fixed_inputs)
-        # plot_some_b0_values(npts_some_b0_values, fixed_inputs, G, B1, flip_angle, target_smoothness, n_slices, shift_targets, path=path)
-        # plot_timeprof(B1, G, fixed_inputs, slice_centers_allB0, half_width, path=path)
-        # plot_phase_fit_error(fixed_inputs, B1, G, slice_centers_allB0, half_width, path=path)
+        export_param_csv(path, path, B1, G, fixed_inputs)
+        plot_some_b0_values(npts_some_b0_values, fixed_inputs, G, B1, flip_angle, target_smoothness, n_slices, shift_targets, path=path)
+        plot_timeprof(B1, G, fixed_inputs, slice_centers_allB0, half_width, path=path)
+        plot_phase_fit_error(fixed_inputs, B1, G, slice_centers_allB0, half_width, path=path)
         plot_off_resonance(B1 + 0j, G, fixed_inputs, freq_offsets_Hz=freq_offsets_Hz, flip_angle=flip_angle, path=path)
     plt.show()
 
 
 if __name__ == "__main__":
-    path = "results/2025-07-06_16-53/train_log.pt"
-    forward(path, npts_some_b0_values=7, Nz=1024, Nt=256, npts_off_resonance=512)
+    path = "results/2025-07-10_10-44/train_log.pt"
+    forward(path, npts_some_b0_values=7, Nz=4096, Nt=1024, npts_off_resonance=512)

params.py
@@ -6,11 +6,11 @@ import numpy as np
 start_epoch = 0
 target_smoothness = 4.0
 shift_targets = False
-epochs = 50000
+epochs = 1000
 resume_from_path = None  # "results/2025-07-05_20-10/train_log.pt"  # path to resume training from
 lr = {"pulse": 5e-5, "gradient": 5e-5}  # learning rate
 plot_loss_frequency = 100  # plot every n steps
-start_logging = 1000  # start logging after n steps
+start_logging = 500  # start logging after n steps
 pre_train_inputs = False  # pre-train on given RF-pulse & gradient
 suppress_loss_peaks = False  # detect peaks in loss function and reduce lr
 loss_metric = "L1"
@@ -27,7 +27,7 @@ loss_weights = {
 }
 
 # BLOCH PARAMETERS:
-n_slices = 1
+n_slices = 2
 n_b0_values = 5
 flip_angle = 0.5 * np.pi
 tfactor = 2  # pulse time is 0.64ms * tfactor
@@ -60,5 +60,5 @@ model_args = {
 scanner_params = {
     "max_gradient": 50,  # mT/m
     "max_diff_gradient": 200,  # mT/m/ms
-    "max_pulse_amplitude": 0.01,  # mT
+    "max_pulse_amplitude": 0.02,  # mT
 }

utils_infer/plot_timeprof.py
@@ -15,9 +15,9 @@ def plot_timeprof(B1, G, fixed_inputs, slice_centers, half_width, path=None):
             color = colors[i]
             timeprof = torch.mean(torch.abs(mxy[b, c - half_width : c + half_width, :]), dim=0)
             com = timeprof[-1] - torch.sum(timeprof)
-            com = int(com.item())
+            comInt = int(com.item())
             ax.plot(fixed_inputs["t_B1"], timeprof, linewidth=0.9, label=f"Slice: {i+1}\ncenter of mass: {fixed_inputs['t_B1'][com].item():.3f}", color=color)
-            ax.plot(fixed_inputs["t_B1"][com], timeprof[com], "o", color=color)
+            ax.plot(fixed_inputs["t_B1"][comInt], timeprof[comInt], "o", color=color)
         plt.legend()
         if path is not None:
             fig.savefig(os.path.join(os.path.dirname(path), f"timeprof_{b}.png"), dpi=300)